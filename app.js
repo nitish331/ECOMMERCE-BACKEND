01eed182427ba3edcd0a7275fe22591abb38fe75
Nitish <nitishkathuria33@gamil.com>
2023-02-04 19:22:21 +0530

    changes done

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ dotenv.config({ path: "backend/config/config.env" });
 
 const errorMiddleware = require("./middlewares/error");
 
+app.use((req, res, next) => {
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers":
+      "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+  });
+
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
